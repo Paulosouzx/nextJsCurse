522486f148fd37c7992ffc002010064c9093d211
paulo Souza <paulosouzx14@gmail.com>
2025-02-19 15:00:39 +0000

    Learn about File Colocation

---
+++ src/app/dashboard/line-chart.tsx
@@ -0,0 +1,5 @@
+//this component not must be visible.
+
+export default function LineChart(){
+    return <h1>not visible</h1>
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function VisibleTestRouter() {
+    return <h1>Visible</h1>
+}
