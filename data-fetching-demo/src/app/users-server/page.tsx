1e54a683c68be308a10b927e72f7652ad52896e9
Paulo Souza <paulosouzx14@gmail.com>
2025-02-25 16:29:54 +0000

    Server fetching Data

---
+++ data-fetching-demo/src/app/users-server/page.tsx
@@ -0,0 +1,30 @@
+type User = {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    phone: string;
+};
+
+export default async function UsersServer() {
+
+    const res = await fetch("https://jsonplaceholder.typicode.com/users")
+    const users: User[] = await res.json();
+    return (
+        <ul className="space-y-4 p-4">
+            {users.map((line) => (
+                <li
+                    key={line.id}
+                    className="p-9 bg-white shadow-md rounded-lg text-gray-700"
+                >
+                    <div className="font-bold">{line.name}</div>
+                    <div className="text-sm">
+                        <div>Username: {line.username}</div>
+                        <div>Email: {line.email}</div>
+                        <div>Phone: {line.phone}</div>
+                    </div>
+                </li>
+            ))}
+        </ul>
+    )
+}
